85e4ef42c360211ba300a13a1b76760e530412ac
leapon <leapon@gmail.com>
2015-08-26 00:18:37 -0400

    drug info converted and saved to drugs.json

--- site/support/readcsv.js
+++ site/support/readcsv.js
@@ -5,6 +5,7 @@ var transform = require('stream-transform');
 var output = [];
 var parser = parse({delimiter: '\t'})
 var input = fs.createReadStream('../public/data/drugbank/drugs.txt');
+var output = fs.createWriteStream('../public/data/drugbank/drugs-output.json');
 var fields = null;
 var transformer = transform(function(record, callback) {
   //console.log('record:', record);
@@ -12,9 +13,12 @@ var transformer = transform(function(record, callback) {
     fields = record;
     console.log('fields:', fields);
   }
-  setTimeout(function(){
-    //callback(null, record.join('|')+'\n');
-    callback(null, '');
+  setTimeout(function() {
+    var drug = {};
+    for (var i = 0; i < fields.length; i++) {
+      drug[fields[i]] = record[i];
+    }
+    callback(null, JSON.stringify(drug) + ',');
   }, 500);
 }, { parallel: 10 });
-input.pipe(parser).pipe(transformer).pipe(process.stdout);
+input.pipe(parser).pipe(transformer).pipe(output);
